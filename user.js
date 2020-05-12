8b9bbb62ec5641c300169dfc7268c0fb4a20951c
Ching Yeung Wallace WONG <wallen435@gmail.com>
2020-05-12 13:32:46 +0800

    add ing the user locatoin

--- user.js
+++ user.js
@@ -127,39 +127,16 @@ function initMap()
             ]
     });
 
-    /*To displays content (usually text or images) in a popup window above the map, 
-    at a given location.*/
-
+    //Geolocation 
     infoWindow = new google.maps.InfoWindow;
 
-    //Geolocation of use location.
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+    //The marker of Google map
+    var marker = new google.maps.Marker
+        (
+            {
+                position: mapProp.center,
+                map: map
+            }
+    );
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
     }
-    else
-    {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
