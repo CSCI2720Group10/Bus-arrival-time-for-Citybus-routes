9988fb80e34a7cd9c7974be932567b6beee89bc1
DESKTOP-ERSHEEJ\Nelson Isaiah Kwan <1155092383@link.cuhk.edu.hk>
2020-05-13 20:57:26 +0800

    Added stepSize for bar chart y-axis

--- user.js
+++ user.js
@@ -442,6 +442,7 @@ $(document).ready(function ()
                                 },
                                 ticks: {
                                     beginAtZero:true,
+                                    stepSize: 1
                                 }
                             }]
                         },
