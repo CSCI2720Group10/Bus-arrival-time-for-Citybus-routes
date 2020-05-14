b443152f7b499416eaaf1db8419c9e28dcf23689
LeeKHRay <49152689+LeeKHRay@users.noreply.github.com>
2020-05-14 22:49:46 +0800

    remove startLocId and endLocId in RouteSchema

--- app.js
+++ app.js
@@ -54,8 +54,6 @@ var User = mongoose.model('User', UserSchema);
                                                                   //Route Schema
 var RouteSchema = mongoose.Schema({
 	routeId: { type: String, required: true },
-	startLocId: { type: Number, required: true },
-	endLocId: { type: Number, required: true },
 	stopCount: { type: Number, required: true },
     dir: { type: String, required: true },
     locInfo: [{ loc: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', required: true },
@@ -73,15 +71,6 @@ var CommentSchema = mongoose.Schema({
     time: { type: String, required: true }
 });
 var Comment= mongoose.model('Comment', CommentSchema);
-
-                                                                //Favourite Location Schema
-var FavouriteLocSchema = mongoose.Schema({
-	locId : { type: Number, required: true, unique: true },
-	favNum: { type: Number, required: true },
-    userIds: [{userId: {type: Number, required: true}}]
-});
-var FavLoc = mongoose.model('FavLoc', FavouriteLocSchema);
-
 
 app.use("/", express.static(__dirname));
 
@@ -767,8 +756,6 @@ app.post("/admin/flush", async function(req, res){
         try{
             var r = new Route({
                 routeId: route.routeId,
-                startLocId: route.startLocId,
-                endLocId: route.endLocId,
                 stopCount: route.stopCount,
                 dir: "I",
                 locInfo: locInInfo[i]});            //locInfo[i]
@@ -785,8 +772,6 @@ app.post("/admin/flush", async function(req, res){
         try{
             var r = new Route({
                 routeId: route.routeId,
-                startLocId: route.startLocId,
-                endLocId: route.endLocId,
                 stopCount: route.stopCount,
                 dir: "O",
                 locInfo: locOutInfo[i]});            //locInfo[i]
@@ -850,8 +835,7 @@ app.post("/admin/location", function(req,res){
                                 Route.updateOne({routeId: req.body['routeId'], dir: req.body['dir']},
                                                 {$push: {locInfo: {loc: l._id,
                                                                    seq: route.locInfo[route.locInfo.length - 1].seq + 1}},
-                                                 $inc: {stopCount: 1},
-                                                 endLocId: req.body['locId']})
+                                                 $inc: {stopCount: 1}})
                                 .exec(function(err, result){
                                     if (err) {
                                         res.send(err);
